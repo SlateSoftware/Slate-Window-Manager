abd6093fac2c14ae5420876073b333ed335dd61f
franzageek <franzageek2020@gmail.com>
2025-06-12 22:37:12 +0200

    Implement cursor; start implementing moving windows

--- sdewm.c
+++ sdewm.c
@@ -1,4 +1,5 @@
 #include <X11/Xlib.h>
+#include <X11/cursorfont.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <intdef.h>
@@ -23,8 +24,10 @@ int main(void)
     }
 
     Window root = DefaultRootWindow(display);
+    Cursor default_cursor = XCreateFontCursor(display, XC_left_ptr);
+    XDefineCursor(display, root, default_cursor);
     XSetErrorHandler(wm_error_handler);
-    XSelectInput(display, root, SubstructureRedirectMask);
+    XSelectInput(display, root, SubstructureRedirectMask | SubstructureNotifyMask);
     XSync(display, false);
     if (wm_detecetd)
     {
@@ -32,4 +35,65 @@ int main(void)
         return EXIT_FAILURE;
     }
     printf("sdewm: running\n");
+    XEvent ev;
+    bool moving = false;
+    Window moving_window = None;
+    int win_x = 0;
+    int win_y = 0;
+    int start_x = 0;
+    int start_y = 0;
+
+    while (true)
+    {
+        XNextEvent(display, &ev);
+        switch (ev.type)
+        {
+            case MapRequest:
+            {
+                XMapWindow(display, ev.xmaprequest.window);
+                XSetInputFocus(display, ev.xmaprequest.window, RevertToPointerRoot, CurrentTime);
+                XSelectInput(display, ev.xmaprequest.window, ButtonPressMask | ButtonReleaseMask | PointerMotionMask);
+                break;
+            }
+            case ButtonPress:
+            {
+                if (ev.xbutton.subwindow != None && ev.xbutton.button == Button1 && (ev.xbutton.state & Mod1Mask))
+                {
+                    moving = true;
+                    moving_window = ev.xbutton.subwindow;
+                    XWindowAttributes attr;
+                    XGetWindowAttributes(display, moving_window, &attr);
+                    win_x = attr.x;
+                    win_y = attr.y;
+                    start_x = ev.xbutton.x_root;
+                    start_y = ev.xbutton.y_root;
+                }
+                break;
+            }
+            case MotionNotify:
+            {
+                if (moving && moving_window != None)
+                {
+                    int dx = ev.xmotion.x_root - start_x;
+                    int dy = ev.xmotion.y_root - start_y;
+                    XMoveWindow(display, moving_window, win_x + dx, win_y + dy);
+                }
+                break;
+            }
+            case ButtonRelease:
+            {
+                if (moving && ev.xbutton.button == Button1)
+                {
+                    moving = false;
+                    moving_window = None;
+                }
+                break;
+            }
+            
+            default:
+                break;
+        }
+    }
+    XCloseDisplay(display);
+    return 0;
 }
